a6925ccdb692e6e248f1c67ba012126a7fbd9027
Ekaterina Trishina <rinarish@mail.ru>
2017-08-22 21:51:20 +0300

    Deleted service test

--- SimplePictureClient/SimplePictureClientTests/PictureServiceTests.m
+++ SimplePictureClient/SimplePictureClientTests/PictureServiceTests.m
@@ -16,40 +16,44 @@
 
 @interface PictureServiceTests : XCTestCase
 
+@property (nonatomic, strong) PictureService *pictureService;
+@property (nonatomic, weak) id<PictureServiceOutput> delegateMock;
+@property (nonatomic, strong) PictureXMLParser *parserMock;
+@property (nonatomic, strong) ImageLoader *loaderMock;
+@property (nonatomic, strong) AFURLSessionManager *managerMock;
+@property (nonatomic, copy) NSURLRequest *requestMock;
+
 @end
 
 @implementation PictureServiceTests
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    self.pictureService = [PictureService new];
+    self.delegateMock = OCMProtocolMock(@protocol(PictureServiceOutput));
+    self.parserMock = OCMClassMock([PictureXMLParser class]);
+    self.loaderMock = OCMClassMock([ImageLoader class]);
+    self.managerMock = OCMClassMock([AFURLSessionManager class]);
+    self.requestMock = OCMClassMock([NSURLRequest class]);
+    
+    self.pictureService.delegate = self.delegateMock;
+    self.pictureService.parser = self.parserMock;
+    self.pictureService.loader = self.loaderMock;
+    self.pictureService.manager = self.managerMock;
+    self.pictureService.request = self.requestMock;
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    self.delegateMock = nil;
+    self.parserMock = nil;
+    self.loaderMock = nil;
+    self.managerMock = nil;
+    self.requestMock = nil;
+    self.pictureService = nil;
+    
     [super tearDown];
 }
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-//- (void)testExample
-//{
-//    Example *exampleOrig = [[Example alloc] init];
-//    id loginCntrl = [OCMockObject mockForClass:[ExampleLC class]];
-//    [[[loginCntrl expect] andDo:^(NSInvocation *invocation) {
-//        void (^successBlock)(NSString *authToken) = [invocation getArgumentAtIndexAsObject:3];
-//        successBlock(@"Dummy");
-//    }] loginWithUserPass:OCMOCK_ANY withSuccess:OCMOCK_ANY failure:OCMOCK_ANY];
-//    exampleOrig->_loginCntrl = loginCntrl;
-//    id example = [OCMockObject partialMockForObject:exampleOrig];
-//    [[example expect] saveToken:@"Dummy"];
-//    [example loginWithUser:@"ABC" andPass:@"DEF"];
-//    [loginCntrl verify];
-//    [example verify];
-//}
 
 - (void)testInit {
     NSURL *testURL = [NSURL URLWithString:@"htpp:/test"];
@@ -60,47 +64,5 @@
     AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
     XCTAssertNotNil(manager);
 }
-
-- (void)testLoadPicture {
-    /**
-     *  Создать мок сервиса и сд
-     */
-    id mockService = [OCMockObject mockForClass:[PictureService class]];
-    OCMVerify([mockService loadPicture]);
-    //проверить что после вызова loadPicture через делегат картинка передается
-    ParseXMLPictureSuccess succesBlock;
-    ParseXMLPictureFailure failureBlock;
-    id mockParser = [OCMockObject mockForClass:[PictureXMLParser class]];
-    [[[mockParser stub] andReturn:succesBlock] parseXMLData:[OCMArg isNotNil] success:[OCMArg isNotNil] failure:[OCMArg isNotNil]];
-    [[mockParser expect] parseXMLData:[OCMArg isNotNil] success:succesBlock failure:failureBlock];
-    OCMVerify([mockParser parseXMLData:[OCMArg isNotNil] success:[OCMArg isNotNil] failure:[OCMArg isNotNil]]);
-    
-    LoadPictureSuccess plSuccesBlock;
-    LoadPictureFailure plFailureBlock;
-    Picture *pic;
-    id mockLoader = [OCMockObject mockForClass:[ImageLoader class]];
-    [[[mockLoader stub] andReturn:pic] loadPictureWithURL:[OCMArg isNotNil] success:[OCMArg isNotNil] failure:[OCMArg isNotNil]];
-    [[mockLoader expect] loadPictureWithURL:[OCMArg isNotNil] success:plSuccesBlock failure:plFailureBlock];
-    
-    id mockManager = [OCMockObject mockForClass:[AFURLSessionManager class]];
-    id mockRequest = [OCMockObject mockForClass:[NSURLRequest class]];
-    
-    
-    /**
-     *  1. создать мок парсера и сделать стаб его метода
-     *  2. проверить что парсер вернул строку (equalobjects или notNil)
-     *  3. создать мок лоадера и сделать стаб его метода
-     *  4. проверить что лоадер вернул картинку (equalobjects или notNil)
-     *  5. проверить что картинка передалась делегату
-     */
-    
-    NSString *expectedURL = @"http://25.media.tumblr.com/tumblr_m0gyw4jDJY1qzfd8bo1_1280.jpg";
-    
-    OCMStub([mockParser parseXMLData:[OCMArg isNotNil] success:[OCMArg invokeBlock] failure:[OCMArg invokeBlock]]);
-    
-    
-}
-
-
 
 @end
